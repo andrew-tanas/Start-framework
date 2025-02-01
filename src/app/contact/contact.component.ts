da6b983cd0ab35e3f252cdb13c8f8ed0442d6381
Andrew Tanas <andrewayman15@gmail.com>
2025-02-01 10:56:12 +0200

    dist

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { log } from 'console';
+
+@Component({
+  selector: 'app-contact',
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  
+}
+
